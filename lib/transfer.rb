64b133f1a05571cb2a1a4d7679247b89df12f75b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-13 19:47:10 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount
+  
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    self.sender.valid? && self.receiver.valid? && @amount <= self.sender.balance
+  end
+
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
+  
 end
